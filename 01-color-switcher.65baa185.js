370e7a0bd5e2879b8e61fdb34b7f55b3146a6e5d
Maksym-Babich <111182214+Maksym-Babich@users.noreply.github.com>
2023-01-28 20:31:38 +0000

    Deploying to gh-pages from @ Maksym-Babich/goit-js-hw-09@9d5317117afaec85f5bb4a899a2c4cf8aa8cbb25 🚀

---
+++ 01-color-switcher.65baa185.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};let e=null;const n=function(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`};t.btnStart.addEventListener("click",(function(){e=setInterval(n,1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStart.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.65baa185.js.map
